56c92dd595f1348408cf3acede12e5c0e16d6d8a
WuJiayiSH <wujiayish@msn.com>
2021-09-24 08:20:11 +0800

    fix compiling

--- tests/js-tests/project/Classes/AppDelegate.cpp
+++ tests/js-tests/project/Classes/AppDelegate.cpp
@@ -25,7 +25,10 @@
 #include "AppDelegate.h"
 
 #include "cocos2d.h"
+#include "extensions/cocos-ext.h"
 #include "audio/include/SimpleAudioEngine.h"
+
+#if (CC_TARGET_PLATFORM != CC_PLATFORM_EMSCRIPTEN)
 #include "scripting/js-bindings/auto/jsb_cocos2dx_3d_auto.hpp"
 #include "scripting/js-bindings/auto/jsb_cocos2dx_3d_extension_auto.hpp"
 #include "scripting/js-bindings/auto/jsb_cocos2dx_auto.hpp"
@@ -78,6 +81,8 @@
 #include "scripting/js-bindings/auto/jsb_cocos2dx_audioengine_auto.hpp"
 #endif
 
+#endif // CC_TARGET_PLATFORM != CC_PLATFORM_EMSCRIPTEN
+
 USING_NS_CC;
 USING_NS_CC_EXT;
 using namespace CocosDenshion;
@@ -113,7 +118,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
-
+#if (CC_TARGET_PLATFORM != CC_PLATFORM_EMSCRIPTEN)
     ScriptingCore* sc = ScriptingCore::getInstance();
     sc->addRegisterCallback(register_all_cocos2dx);
     sc->addRegisterCallback(register_cocos2dx_js_core);
@@ -185,7 +190,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     ScriptEngineManager::getInstance()->setScriptEngine(sc);
 
     sc->runScript("main.js");
-
+#endif // CC_TARGET_PLATFORM != CC_PLATFORM_EMSCRIPTEN
     return true;
 }
 
