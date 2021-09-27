35193071fd8046398b07c878b23d03a6edb6bd14
WuJiayiSH <wujiayish@msn.com>
2021-09-28 07:54:30 +0800

    regen

--- cocos/scripting/bindings/ccbind_cocos2dx_network.cpp
+++ cocos/scripting/bindings/ccbind_cocos2dx_network.cpp
@@ -6,7 +6,6 @@ using namespace std;
 using namespace std::placeholders;
 using namespace cocos2d;
 using namespace cocos2d::bindings;
-using namespace cocos2d::network;
 
 COCOS_BINDINGS(ccbind_cocos2dx_network) {
 
@@ -14,15 +13,14 @@ COCOS_BINDINGS(ccbind_cocos2dx_network) {
   class_<Downloader>("jsb.Downloader")
         // TODO: overloaded constructor
     // TODO: Emscripten cannot automatically call destructor.
-    .function("setOnTaskError", &Downloader::setOnTaskError, allow_raw_pointers())
-    .function("setOnTaskProgress", &Downloader::setOnTaskProgress, allow_raw_pointers())
-    .function("createDownloadFileTask", &Downloader::createDownloadFileTask, allow_raw_pointers())
+    .function("setOnTaskError", &Downloader::setOnTaskError)
+    .function("setOnTaskProgress", &Downloader::setOnTaskProgress)
+    .function("createDownloadFileTask", &Downloader::createDownloadFileTask)
     .function("createDownloadFileTask", optional_override(
         [](Downloader& this_, const std::string& arg0, const std::string& arg1){
         return this_.createDownloadFileTask(arg0, arg1);
-      }), allow_raw_pointers())
-    .function("setOnFileTaskSuccess", &Downloader::setOnFileTaskSuccess, allow_raw_pointers())
-    .function("setOnDataTaskSuccess", &Downloader::setOnDataTaskSuccess, allow_raw_pointers())
+      }))
+    .function("setOnFileTaskSuccess", &Downloader::setOnFileTaskSuccess)
+    .function("setOnDataTaskSuccess", &Downloader::setOnDataTaskSuccess)
     .property("_className",  optional_override([](const Downloader& _) -> std::string {return "Downloader";}))    
-    ;
-}
+    ;}
--- cocos/scripting/bindings/ccbind_cocos2dx_spine.cpp
+++ cocos/scripting/bindings/ccbind_cocos2dx_spine.cpp
@@ -1,51 +1,53 @@
 #include "scripting/bindings/CCScriptBindings.h"
 #include "scripting/bindings/ccbind_cocos2dx_spine.hpp"
 #include "editor-support/spine/spine-cocos2dx.h"
+#include "scripting/js-bindings/manual/spine/jsb_cocos2dx_spine_manual.h"
 
 using namespace std;
 using namespace std::placeholders;
 using namespace cocos2d;
 using namespace cocos2d::bindings;
-using namespace spine;
 
 COCOS_BINDINGS(ccbind_cocos2dx_spine) {
 
 
   class_<SkeletonRenderer, base<Node>>("sp.Skeleton")
         // TODO: overloaded constructor
-    .function("setTimeScale", &SkeletonRenderer::setTimeScale, allow_raw_pointers())
-    .function("getDebugSlotsEnabled", &SkeletonRenderer::getDebugSlotsEnabled, allow_raw_pointers())
+    .function("setTimeScale", &SkeletonRenderer::setTimeScale)
+    .function("getDebugSlotsEnabled", &SkeletonRenderer::getDebugSlotsEnabled)
     .function("setAttachment", select_overload<bool(const std::string&, const char*)>(&SkeletonRenderer::setAttachment), allow_raw_pointers())
     // TODO: Only support function overloading with different number of parameters
-    .function("setBonesToSetupPose", &SkeletonRenderer::setBonesToSetupPose, allow_raw_pointers())
+    .function("setBonesToSetupPose", &SkeletonRenderer::setBonesToSetupPose)
     .function("initWithData", &SkeletonRenderer::initWithData, allow_raw_pointers())
     .function("initWithData", optional_override(
         [](SkeletonRenderer& this_, spSkeletonData* arg0){
         return this_.initWithData(arg0);
       }), allow_raw_pointers())
-    .function("setDebugSlotsEnabled", &SkeletonRenderer::setDebugSlotsEnabled, allow_raw_pointers())
-    .function("initWithJsonFile", select_overload<void(const std::string&, const std::string&, float)>(&SkeletonRenderer::initWithJsonFile), allow_raw_pointers())
+    .function("setDebugSlotsEnabled", &SkeletonRenderer::setDebugSlotsEnabled)
+    .function("initWithJsonFile", select_overload<void(const std::string&, const std::string&, float)>(&SkeletonRenderer::initWithJsonFile))
     // TODO: Only support function overloading with different number of parameters
     // TODO: Only support function overloading with different number of parameters
     // TODO: Only support function overloading with different number of parameters
-    .function("setSlotsToSetupPose", &SkeletonRenderer::setSlotsToSetupPose, allow_raw_pointers())
-    .function("initWithBinaryFile", select_overload<void(const std::string&, const std::string&, float)>(&SkeletonRenderer::initWithBinaryFile), allow_raw_pointers())
+    .function("setSlotsToSetupPose", &SkeletonRenderer::setSlotsToSetupPose)
+    .function("initWithBinaryFile", select_overload<void(const std::string&, const std::string&, float)>(&SkeletonRenderer::initWithBinaryFile))
     // TODO: Only support function overloading with different number of parameters
     // TODO: Only support function overloading with different number of parameters
     // TODO: Only support function overloading with different number of parameters
-    .function("setToSetupPose", &SkeletonRenderer::setToSetupPose, allow_raw_pointers())
-    .function("setDebugMeshesEnabled", &SkeletonRenderer::setDebugMeshesEnabled, allow_raw_pointers())
-    .function("isTwoColorTint", &SkeletonRenderer::isTwoColorTint, allow_raw_pointers())
-    .function("initialize", &SkeletonRenderer::initialize, allow_raw_pointers())
-    .function("setDebugBonesEnabled", &SkeletonRenderer::setDebugBonesEnabled, allow_raw_pointers())
-    .function("getDebugBonesEnabled", &SkeletonRenderer::getDebugBonesEnabled, allow_raw_pointers())
-    .function("getTimeScale", &SkeletonRenderer::getTimeScale, allow_raw_pointers())
-    .function("setTwoColorTint", &SkeletonRenderer::setTwoColorTint, allow_raw_pointers())
-    .function("getDebugMeshesEnabled", &SkeletonRenderer::getDebugMeshesEnabled, allow_raw_pointers())
+    .function("setToSetupPose", &SkeletonRenderer::setToSetupPose)
+    .function("setDebugMeshesEnabled", &SkeletonRenderer::setDebugMeshesEnabled)
+    .function("isTwoColorTint", &SkeletonRenderer::isTwoColorTint)
+    .function("getBlendFunc", &SkeletonRenderer::getBlendFunc)
+    .function("initialize", &SkeletonRenderer::initialize)
+    .function("setDebugBonesEnabled", &SkeletonRenderer::setDebugBonesEnabled)
+    .function("getDebugBonesEnabled", &SkeletonRenderer::getDebugBonesEnabled)
+    .function("getTimeScale", &SkeletonRenderer::getTimeScale)
+    .function("setTwoColorTint", &SkeletonRenderer::setTwoColorTint)
+    .function("getDebugMeshesEnabled", &SkeletonRenderer::getDebugMeshesEnabled)
+    .function("setBlendFunc", &SkeletonRenderer::setBlendFunc)
     .function("setVertexEffect", &SkeletonRenderer::setVertexEffect, allow_raw_pointers())
     .function("setSkin", select_overload<bool(const char*)>(&SkeletonRenderer::setSkin), allow_raw_pointers())
     // TODO: Only support function overloading with different number of parameters
-    .function("updateWorldTransform", &SkeletonRenderer::updateWorldTransform, allow_raw_pointers())
+    .function("updateWorldTransform", &SkeletonRenderer::updateWorldTransform)
     .function("getSkeleton", &SkeletonRenderer::getSkeleton, allow_raw_pointers())
     .function("drawDebug", &SkeletonRenderer::drawDebug, allow_raw_pointers())
     .class_function("create", select_overload<spine::SkeletonRenderer*(const std::string&, spAtlas*, float)>(&SkeletonRenderer::createWithFile), allow_raw_pointers())
@@ -61,45 +63,43 @@ COCOS_BINDINGS(ccbind_cocos2dx_spine) {
     .constructor<>()
     .function("setTrackCompleteListener", &SkeletonAnimation::setTrackCompleteListener, allow_raw_pointers())
     .function("findAnimation", &SkeletonAnimation::findAnimation, allow_raw_pointers())
-    .function("setCompleteListener", &SkeletonAnimation::setCompleteListener, allow_raw_pointers())
-    .function("setMix", &SkeletonAnimation::setMix, allow_raw_pointers())
+    .function("setCompleteListener", &SkeletonAnimation::setCompleteListener)
+    .function("setMix", &SkeletonAnimation::setMix)
     .function("setTrackStartListener", &SkeletonAnimation::setTrackStartListener, allow_raw_pointers())
     .function("addEmptyAnimation", &SkeletonAnimation::addEmptyAnimation, allow_raw_pointers())
     .function("addEmptyAnimation", optional_override(
         [](SkeletonAnimation& this_, int arg0, float arg1){
         return this_.addEmptyAnimation(arg0, arg1);
       }), allow_raw_pointers())
-    .function("setDisposeListener", &SkeletonAnimation::setDisposeListener, allow_raw_pointers())
+    .function("setDisposeListener", &SkeletonAnimation::setDisposeListener)
     .function("setTrackInterruptListener", &SkeletonAnimation::setTrackInterruptListener, allow_raw_pointers())
     .function("onTrackEntryEvent", &SkeletonAnimation::onTrackEntryEvent, allow_raw_pointers())
-    .function("setEndListener", &SkeletonAnimation::setEndListener, allow_raw_pointers())
+    .function("setEndListener", &SkeletonAnimation::setEndListener)
     .function("getState", &SkeletonAnimation::getState, allow_raw_pointers())
     .function("setTrackDisposeListener", &SkeletonAnimation::setTrackDisposeListener, allow_raw_pointers())
-    .function("setEventListener", &SkeletonAnimation::setEventListener, allow_raw_pointers())
+    .function("setEventListener", &SkeletonAnimation::setEventListener)
     .function("setEmptyAnimation", &SkeletonAnimation::setEmptyAnimation, allow_raw_pointers())
     .function("setTrackEventListener", &SkeletonAnimation::setTrackEventListener, allow_raw_pointers())
-    .function("clearTrack", &SkeletonAnimation::clearTrack, allow_raw_pointers())
+    .function("clearTrack", &SkeletonAnimation::clearTrack)
     .function("clearTrack", optional_override(
         [](SkeletonAnimation& this_){
         return this_.clearTrack();
-      }), allow_raw_pointers())
-    .function("setInterruptListener", &SkeletonAnimation::setInterruptListener, allow_raw_pointers())
+      }))
+    .function("setInterruptListener", &SkeletonAnimation::setInterruptListener)
     .function("onAnimationStateEvent", &SkeletonAnimation::onAnimationStateEvent, allow_raw_pointers())
-    .function("setEmptyAnimations", &SkeletonAnimation::setEmptyAnimations, allow_raw_pointers())
-    .function("clearTracks", &SkeletonAnimation::clearTracks, allow_raw_pointers())
+    .function("setEmptyAnimations", &SkeletonAnimation::setEmptyAnimations)
+    .function("clearTracks", &SkeletonAnimation::clearTracks)
     .function("setTrackEndListener", &SkeletonAnimation::setTrackEndListener, allow_raw_pointers())
-    .function("setStartListener", &SkeletonAnimation::setStartListener, allow_raw_pointers())
+    .function("setStartListener", &SkeletonAnimation::setStartListener)
     .function("ctor", &cc_bindings_ctor<SkeletonAnimation>, allow_raw_pointers())
     .class_function("createWithBinaryFile", select_overload<spine::SkeletonAnimation*(const std::string&, const std::string&, float)>(&SkeletonAnimation::createWithBinaryFile), allow_raw_pointers())
     // TODO: Only support function overloading with different number of parameters
     // TODO: Only support function overloading with different number of parameters
     // TODO: Only support function overloading with different number of parameters
-    .class_function("create", select_overload<spine::SkeletonAnimation*()>(&SkeletonAnimation::create), allow_raw_pointers())
     .class_function("createWithJsonFile", select_overload<spine::SkeletonAnimation*(const std::string&, const std::string&, float)>(&SkeletonAnimation::createWithJsonFile), allow_raw_pointers())
     // TODO: Only support function overloading with different number of parameters
     // TODO: Only support function overloading with different number of parameters
     // TODO: Only support function overloading with different number of parameters
     .property("_className",  optional_override([](const SkeletonAnimation& _) -> std::string {return "SkeletonAnimation";}))    
     // TODO: assign cc.Class.extend to sp.SkeletonAnimation.extend
-    ;
-}
+    ;}
