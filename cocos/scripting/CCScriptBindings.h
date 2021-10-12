e678d1a48c31dbea1461228ed394d523d942e881
WuJiayiSH <wujiayish@msn.com>
2021-10-12 15:08:15 +0800

    Custom marshal for vector<T> to JSArray

--- cocos/scripting/CCScriptBindings.h
+++ cocos/scripting/CCScriptBindings.h
@@ -128,4 +128,35 @@ namespace emscripten {                                            \
     }                                                             \
 }
 
+// Custom marshal vector<T> to JSArray
+namespace emscripten {
+namespace internal {
+
+template <typename T, typename Allocator>
+struct BindingType<std::vector<T, Allocator>> {
+    using ValBinding = BindingType<val>;
+    using WireType = ValBinding::WireType;
+
+    static WireType toWireType(const std::vector<T, Allocator> &vec) {
+        return ValBinding::toWireType(val::array(vec));
+    }
+
+    static std::vector<T, Allocator> fromWireType(WireType value) {
+        return vecFromJSArray<T>(ValBinding::fromWireType(value));
+    }
+};
+
+
+template <typename T>
+struct TypeID<T,
+              typename std::enable_if<std::is_same<
+                  typename Canonicalized<T>::type,
+                  std::vector<typename Canonicalized<T>::type::value_type,
+                              typename Canonicalized<T>::type::allocator_type>>::value>::type> {
+    static constexpr TYPEID get() { return TypeID<val>::get(); }
+};
+
+}  // namespace internal
+}  // namespace emscripten
+
 #endif // __CC_SCRIPT_BINDINGS_H__
