8cfdcc4b8bf63e8487ba48c9b473ed448d7706b3
WuJiayiSH <wujiayish@msn.com>
2021-10-29 19:50:27 +0800

    spine manual API(0)

--- cocos/scripting/javascript-bindings/jsb_cocos2dx_spine.cpp
+++ cocos/scripting/javascript-bindings/jsb_cocos2dx_spine.cpp
@@ -10,6 +10,130 @@ using namespace spine;
 
 COCOS_BINDINGS(jsb_cocos2dx_spine) {
 
+  value_object<spEventData>("_.spEventData")
+    .field("name", 
+      optional_override([](const spEventData& this_) -> std::string {return this_.name;}), 
+      optional_override([](const spEventData& this_, const std::string& arg0) {CCLOG("spEventData.name is read-only");}))
+    .field("intValue", &spEventData::intValue)
+    .field("floatValue", &spEventData::floatValue)
+    .field("stringValue", 
+      optional_override([](const spEventData& this_) -> std::string {return this_.name;}), 
+      optional_override([](const spEventData& this_, const std::string& arg0) {CCLOG("spEventData.stringValue is read-only");}))
+    ;
+/*
+  value_object<spEvent>("_.spEvent")
+    .field("data", &spEvent::data)
+    .field("intValue", &spEvent::intValue)
+    .field("floatValue", &spEvent::floatValue)
+    .field("stringValue", &spEvent::stringValue)
+    ;
+
+  value_object<spBoneData>("_.spBoneData")
+    .field("parent", &spBoneData::parent)
+    .field("index", &spBoneData::index)
+    .field("name", &spBoneData::name)
+    .field("parent", &spBoneData::parent)
+    .field("length", &spBoneData::length)
+    .field("x", &spBoneData::x)
+    .field("y", &spBoneData::y)
+    .field("rotation", &spBoneData::rotation)
+    .field("scaleX", &spBoneData::scaleX)
+    .field("scaleY", &spBoneData::scaleY)
+    .field("shearX", &spBoneData::shearX)
+    .field("shearY", &spBoneData::shearY)
+    .field("transformMode", &spBoneData::transformMode)
+    ;
+
+  value_object<spBone>("_.spBone")
+    .field("data", &spBone::data)
+    .field("parent", &spBone::parent)
+    .field("x", &spBone::x)
+    .field("y", &spBone::y)
+    .field("rotation", &spBone::rotation)
+    .field("scaleX", &spBone::scaleX)
+    .field("scaleY", &spBone::scaleY)
+    .field("shearX", &spBone::shearX)
+    .field("shearY", &spBone::shearY)
+    .field("m00", &spBone::a)
+    .field("m01", &spBone::b)
+    .field("worldX", &spBone::worldX)
+    .field("m10", &spBone::c)
+    .field("m11", &spBone::d)
+    .field("worldY", &spBone::worldY)
+    ;
+
+  value_object<spSkeleton>("_.spSkeleton")
+    .field("x", &spSkeleton::x)
+    .field("y", &spSkeleton::y)
+    .field("flipX", &spSkeleton::flipX)
+    .field("flipY", &spSkeleton::flipY)
+    .field("time", &spSkeleton::time)
+    .field("boneCount", &spSkeleton::bonesCount)
+    .field("slotCount", &spSkeleton::slotsCount)
+    ;
+
+  value_object<spAttachment>("_.spAttachment")
+    .field("name", &spAttachment::name)
+    .field("type", &spAttachment::type)
+    ;
+
+  value_object<spColor>("_.spColor")
+    .field("r", &spColor::r)
+    .field("g", &spColor::g)
+    .field("b", &spColor::b)
+    ;
+
+  value_object<spSlotData>("_.spSlotData")
+    .field("name", &spSlotData::name)
+    .field("attachmentName", &spSlotData::attachmentName)
+    .field("color", &spSlotData::color)
+    .field("darkColor", &spSlotData::darkColor)
+    .field("blendMode", &spSlotData::blendMode)
+    .field("boneData", &spSlotData::boneData)
+    .field("index", &spSlotData::index)
+    ;
+
+  value_object<spSlot>("_.spSlot")
+    .field("color", &spSlot::color)
+    .field("darkColor", &spSlot::darkColor)
+    .field("bone", &spSlot::bone)
+    .field("attachment", &spSlot::attachment)
+    .field("data", &spSlot::data)
+    .field("attachmentVerticesCapacity", &spSlot::attachmentVerticesCapacity)
+    .field("attachmentVerticesCount", &spSlot::attachmentVerticesCount)
+    .field("attachmentVertices", 
+    optional_override(
+      [](const spSlot& this_)
+      {
+        std::vector<float> ret(this_.attachmentVertices, this_.attachmentVertices + this_.attachmentVerticesCount);
+        return ret;
+      }
+    ), 
+    optional_override(
+      [](const spSlot& this_, const std::vector<float> arg0)
+      {
+        
+      }
+    )
+    )
+    ;
+
+  value_object<spTimeline>("_.spTimeline")
+    .field("type", &spTimeline::type)
+    ;
+
+  value_object<spAnimationState>("_.spAnimationState")
+    .field("timeScale", &spAnimationState::timeScale)
+    .field("trackCount", &spAnimationState::tracksCount)
+    ;
+
+  value_object<spAnimation>("_.spAnimation")
+    .field("duration", &spAnimation::duration)
+    .field("timelineCount", &spAnimation::timelinesCount)
+    .field("name", &spAnimation::name)
+    .field("timelines", &spAnimation::timelines)
+    ;
+*/
 
   class_<SkeletonRenderer, base<Node>>("sp.Skeleton")
         // TODO: overloaded constructor
@@ -89,6 +213,13 @@ COCOS_BINDINGS(jsb_cocos2dx_spine) {
     .function("updateWorldTransform", &SkeletonRenderer::updateWorldTransform)
     .function("getSkeleton", &SkeletonRenderer::getSkeleton, allow_raw_pointers())
     .function("drawDebug", &SkeletonRenderer::drawDebug, allow_raw_pointers())
+    // from manual
+    .function("findBone", &SkeletonRenderer::findBone, allow_raw_pointers())
+    .function("findSlot", &SkeletonRenderer::findSlot, allow_raw_pointers())
+    .function("setDebugBones", &SkeletonRenderer::setDebugBonesEnabled)
+    .function("setDebugSolots", &SkeletonRenderer::setDebugSlotsEnabled)
+    .function("getAttachment", &SkeletonRenderer::getAttachment, allow_raw_pointers())
+    // end of manual
     .class_function("create", optional_override(
         [](const std::string& arg0, const val& arg1){
 		  if (arg1.isString())
@@ -146,6 +277,11 @@ COCOS_BINDINGS(jsb_cocos2dx_spine) {
     .function("clearTracks", &SkeletonAnimation::clearTracks)
     .function("setTrackEndListener", &SkeletonAnimation::setTrackEndListener, allow_raw_pointers())
     .function("setStartListener", &SkeletonAnimation::setStartListener)
+    // from manual
+    .function("getCurrent", &SkeletonAnimation::getCurrent, allow_raw_pointers())
+    .function("setAnimation", &SkeletonAnimation::setAnimation, allow_raw_pointers())
+    .function("addAnimation", &SkeletonAnimation::addAnimation, allow_raw_pointers())
+    // end of manual
     .class_function("createWithBinaryFile", optional_override(
         [](const std::string& arg0, const val& arg1){
 		  if (!arg1.isString())
